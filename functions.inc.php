91a4893f720c6e6e353df8c07adc91723a4bfdba
Alain <alainmenag@gmail.com>
2025-10-09 18:56:50 -0400

    Added wrapper

--- functions.inc.php
+++ functions.inc.php
@@ -1,24 +1,27 @@
 <?php
 // functions.inc.php
 
-function Oryk_gui_configpageinit($page)
+if (!function_exists('Oryk_gui_configpageinit'))
 {
-	$freepbx = \FreePBX::create();
-	$oryk = $freepbx->oryk_gui;
+	function Oryk_gui_configpageinit($page)
+	{
+		$freepbx = \FreePBX::create();
+		$oryk = $freepbx->oryk_gui;
 
-	$user = $oryk->getUser();
+		$user = $oryk->getUser();
 
-	// If logged in and module no module access, load assets
-	if ($user && !$user['admin'] && !$_SESSION['AMP_user']->checkSection('oryk_gui')) {
-		echo '<link rel="stylesheet" type="text/css" href="/admin/assets/oryk_gui/admin/reset.css">';
-		echo '<script type="text/javascript" src="/admin/assets/oryk_gui/admin/extend.js"></script>';
+		// If logged in and module no module access, load assets
+		if ($user && !$user['admin'] && !$_SESSION['AMP_user']->checkSection('oryk_gui')) {
+			echo '<link rel="stylesheet" type="text/css" href="/admin/assets/oryk_gui/admin/reset.css">';
+			echo '<script type="text/javascript" src="/admin/assets/oryk_gui/admin/extend.js"></script>';
+		}
+
+		echo '<script type="text/javascript" src="/admin/assets/oryk_gui/vendor/touch-scroll.js"></script>';
+
+		// print all $freepbx keys
+		// die(json_encode([
+		// 	'freepbx' => array_keys(get_object_vars($freepbx)),
+		// ]));
 	}
-
-	echo '<script type="text/javascript" src="/admin/assets/oryk_gui/vendor/touch-scroll.js"></script>';
-
-	// print all $freepbx keys
-	// die(json_encode([
-	// 	'freepbx' => array_keys(get_object_vars($freepbx)),
-	// ]));
 }
 ?>
